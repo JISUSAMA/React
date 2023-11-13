9c71bf3f41458751ce63ee03f7c1c48abc48a46c
PPsom <j2su0218@gmail.com>
2023-11-13 19:54:13 +0900

    202311.13

--- Grammar/grammar-2/src/App.js
+++ Grammar/grammar-2/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <div className='black-nav'>
-        <h4 id={post} style={{color :'red'}} >블로그</h4>
+      <div className="black-nav">
+        <h4 id={post} style={{ color: "red" }}>
+          블로그
+        </h4>
+      </div>
+      <div className="grammar-list">
+        <h4>글 제목</h4>
+        <p>2023-11-13</p>
       </div>
       <h5>{post}</h5>
     </div>
